050a207f8635b91ec4e8ef99d31623280862f3af
zpf <zhuangpengfei@qq.com>
2019-11-13 10:08:41 +0800

    日考10

---
+++ src/main/java/com/bw/utils/DateUtil.java
@@ -0,0 +1,17 @@
+package com.bw.utils;
+
+import java.util.Date;
+
+public class DateUtil {
+	//根据传入的日期获取年龄
+	public static int getAge(Date src){
+		
+		return 0;
+	}
+	
+	//根据传入的参数获取该日期的月初日期，例如给定的日期为“2019-09-19 19:29:39”，返回“2019-09-01 00:00:00”
+	public static Date getDateByMonthInit(Date src){
+		
+		return src;
+	}
+}
